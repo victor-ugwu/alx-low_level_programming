4b2fc730511aea129ed31119e292ab4b766a915c
victor-ugwu <vugwu7119@gmail.com>
2022-10-13 16:59:26 -0700

    yes

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * char: 'the program's variable
+ *
+ * puts: 'calling the variable'
+ *
+ * Return: Always zero (success)
+ */
+int main(void)
+{
+	char Myfirst[] = "Programming is like building a multilingual puzzle \n";
+
+	puts(Myfirst);
+
+	return (0);
+}
