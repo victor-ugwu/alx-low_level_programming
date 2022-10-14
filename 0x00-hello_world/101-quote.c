a17eee07ddc65b59b8522b4eb5bed77c034c4794
victor-ugwu <vugwu7119@gmail.com>
2022-10-13 19:31:19 -0700

    dd

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<unistd.h>
+/**
+ * main - Entry point
+ *
+ * Write: standered error output
+ *
+ * Return: one
+*/
+int main(void)
+{
+	write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19",
+	59);
+
+	return (1);
+}
