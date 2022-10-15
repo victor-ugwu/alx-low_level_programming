1f47bb35dd9406bc7c7290928c8d62f0e3674cfa
victor-ugwu <vugwu7119@gmail.com>
2022-10-14 20:40:12 -0700

    dd

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,9 +7,8 @@
  *
  * body: output the value on n
  *
- * Return zero
+ * Return: zero
  */
-
 int main(void)
 {
 	int n;
@@ -23,8 +22,8 @@ int main(void)
 	else if (n == 0)
 		printf("%d is zero\n", n);
 
-	else 
-		printf("%d is negative", n);
+	else
+		printf("%d is negative\n", n);
 
 	return (0);
 }
