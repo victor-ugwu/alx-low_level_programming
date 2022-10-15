47a87fa32fe01e3b83c586d7a101b15252d44a53
victor-ugwu <vugwu7119@gmail.com>
2022-10-15 12:19:25 -0700

    ee

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * putchar: print combination of all double digits
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int number;
+	int number2;
+
+	for (number = 0; number < 10; number++)
+	{
+		for (number2 = 0; number2 < 10; number2++)
+		{
+			putchar((number % 10) + '0');
+			putchar((number2 % 10) + '0');
+
+			if (number == 9 && number2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
