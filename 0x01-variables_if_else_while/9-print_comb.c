2be4d7c22bdcbe9c9756424147b48ec3d667b43f
victor-ugwu <vugwu7119@gmail.com>
2022-10-15 11:55:32 -0700

    ff

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,9 +10,12 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; number == 9; number++)
+	for (number = 0; number <= 9; number++)
 	{
 		putchar((number % 10) + '0');
+
+		if (number == 9)
+			continue;
 
 		putchar(',');
 		putchar(' ');
