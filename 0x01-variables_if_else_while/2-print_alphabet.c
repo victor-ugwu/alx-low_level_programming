152ee3bac5b37c1b12ec885f4f843c48ee929cd1
victor-ugwu <vugwu7119@gmail.com>
2022-10-15 04:32:06 -0700

    dd

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <ctype.h>
 /**
  * main - Entry point
  *
@@ -11,7 +11,9 @@ int main(void)
 {
 	for (int lower = 'A'; lower <= 'Z'; lower++)
 	{
-		putchar(tolower(lower));
+		char pv = tolower(lower);
+
+		putchar(pv);
 	}
 
 	return (0);
