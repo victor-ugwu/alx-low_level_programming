a078a607a51cc3d96a8e079aad60cdc159ffd668
victor-ugwu <vugwu7119@gmail.com>
2022-10-15 06:48:36 -0700

    df

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * print: numbers
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		printf("%d", number);
+	}
+
+	printf("\n");
+
+	return (0);
+}
