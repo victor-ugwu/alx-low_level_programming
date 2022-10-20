b58d9694020abc8189f6d07f87ec73c1faacdd76
victor-ugwu <vugwu7119@gmail.com>
2022-10-20 05:47:31 -0700

    zero task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Checks for negative and positive numbers
+ * 
+ * Return: zero
+ */
+
+int main(void)
+{
+	positive_or_negative(0);
+
+	return (0);
+}
