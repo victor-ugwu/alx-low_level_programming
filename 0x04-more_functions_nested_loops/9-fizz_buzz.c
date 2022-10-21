db2f000dca0e497de1df2099b408a662268a5774
victor-ugwu <vugwu7119@gmail.com>
2022-10-21 14:47:41 -0700

    FIZZBUZZ

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print fizzbuzz
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+
+			printf("Fizz");
+
+		else if ((i % 5) == 0)
+
+			printf("Buzz");
+
+		else if ((i % 3) == 0 && (i % 5) == 0)
+
+			printf("FizzBuzz");
+
+		else
+
+			printf("%d", i);
+
+		if (i == 100)
+			continue;
+		printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Project on more nested loop functions
