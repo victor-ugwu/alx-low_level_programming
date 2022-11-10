b1cb868fd6bb50df76961fc9325647c4e10f7687
victor-ugwu <vugwu7119@gmail.com>
2022-11-10 10:19:12 -0800

    Write a function that concatenates two strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: string lenght
+ * Return: concatnated string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i;
+	unsigned int lenght = n;
+	char *concat;
+
+	if (s1 == NULL)
+		s1 = " ";
+
+	if (s2 == NULL)
+		s2 = " ";
+
+	for (i = 0; s1[i]; i++)
+		lenght++;
+
+	if (concat == NULL)
+		return (NULL);
+
+	lenght = 0;
+
+	for (i = 0; s1[i]; i++)
+		concat[lenght++] = s1[i];
+
+	for (i = 0; s2[i] && i < n; i++)
+		concat[lenght++] = s2[i];
+
+	concat[lenght] = '\o';
+
+	return (concat);
+}
+
+
