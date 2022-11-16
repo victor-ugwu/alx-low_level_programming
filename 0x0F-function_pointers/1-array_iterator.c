240c7f26e42f015159cd83787d143763468e6aff
victor-ugwu <vugwu7119@gmail.com>
2022-11-16 13:15:09 -0800

    task 1 corected

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,8 @@
-#include "function_pointer.h"
+#include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter on each element of an array
+ * array_iterator - executes a function given as a
+ * parameter on each element of an array
  * @array: the array
  * @size: array size
  * @action: points to the function to be executed
